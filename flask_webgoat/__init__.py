d3da43934ced75fbed2bece9df02b933ae8bde23
Magno Gomes <117935539+ongamse@users.noreply.github.com>
2024-08-01 15:13:06 -0400

    Fixing flask_webgoat/__init__.py for finding 12

--- flask_webgoat/__init__.py
+++ flask_webgoat/__init__.py
@@ -1,58 +1,52 @@
-import os
-import sqlite3
-from pathlib import Path
-
-from flask import Flask, g
-
-DB_FILENAME = "database.db"
-
-
-def query_db(query, args=(), one=False, commit=False):
+	import os
 	import sqlite3
-	from flask import g
+	from pathlib import Path
 
-	DB_FILENAME = 'database.db'
+	from flask import Flask, g
 
-	def get_db():
-	    db = getattr(g, '_database', None)
-	    if db is None:
-	        db = g._database = sqlite3.connect(DB_FILENAME)
-	    return db
+	DB_FILENAME = "database.db"
 
-	def query_db(query, args=(), one=False):
-	    cur = get_db().execute(query, args)
-	    rv = cur.fetchall()
-	    return (rv[0] if rv else None) if one else rv
 
-    app = Flask(__name__)
-    app.secret_key = "aeZ1iwoh2ree2mo0Eereireong4baitixaixu5Ee"
+	def query_db(query, args=(), one=False, commit=False):
+	    with sqlite3.connect(DB_FILENAME) as conn:
+	        cur = conn.cursor().execute(query, args)
+	        if commit:
+	            conn.commit()
+	        return cur.fetchone() if one else cur.fetchall()
 
-    db_path = Path(DB_FILENAME)
-    if db_path.exists():
-        db_path.unlink()
 
-    conn = sqlite3.connect(DB_FILENAME)
-    create_table_query = """CREATE TABLE IF NOT EXISTS user
-    (id INTEGER PRIMARY KEY, username TEXT, password TEXT, access_level INTEGER)"""
-    conn.execute(create_table_query)
+	def create_app():
+	    app = Flask(__name__)
+	    app.secret_key = "aeZ1iwoh2ree2mo0Eereireong4baitixaixu5Ee"
 
-    insert_admin_query = """INSERT INTO user (id, username, password, access_level)
-    VALUES (1, 'admin', 'maximumentropy', 0)"""
-    conn.execute(insert_admin_query)
-    conn.commit()
-    conn.close()
+	    db_path = Path(DB_FILENAME)
+	    if db_path.exists():
+	        db_path.unlink()
 
-    with app.app_context():
-        from . import actions
-        from . import auth
-        from . import status
-        from . import ui
-        from . import users
+	    conn = sqlite3.connect(DB_FILENAME)
+	    create_table_query = """CREATE TABLE IF NOT EXISTS user
+	    (id INTEGER PRIMARY KEY, username TEXT, password TEXT, access_level INTEGER)"""
+	    conn.execute(create_table_query)
 
-        app.register_blueprint(actions.bp)
-        app.register_blueprint(auth.bp)
-        app.register_blueprint(status.bp)
-        app.register_blueprint(ui.bp)
-        app.register_blueprint(users.bp)
-        return app
+	    insert_admin_query = """INSERT INTO user (id, username, password, access_level)
+	    VALUES (1, 'admin', 'maximumentropy', 0)"""
+	    conn.execute(insert_admin_query)
+	    conn.commit()
+	    conn.close()
+
+	    with app.app_context():
+	        from . import actions
+	        from . import auth
+	        from . import status
+	        from . import ui
+	        from . import users
+
+	        app.register_blueprint(actions.bp)
+	        app.register_blueprint(auth.bp)
+	        app.register_blueprint(status.bp)
+	        app.register_blueprint(ui.bp)
+	        app.register_blueprint(users.bp)
+	        return app
+
+
 
