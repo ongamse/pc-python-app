de2b9f2b58fbdf0579ce5caacdfb2c20cb7b253a
Magno Gomes <117935539+ongamse@users.noreply.github.com>
2024-08-05 13:52:02 -0400

    Fixing flask_webgoat/__init__.py for finding 11

--- flask_webgoat/__init__.py
+++ flask_webgoat/__init__.py
@@ -8,16 +8,17 @@ DB_FILENAME = "database.db"
 
 
 def query_db(query, args=(), one=False, commit=False):
-    with sqlite3.connect(DB_FILENAME) as conn:
-        # vulnerability: Sensitive Data Exposure
-        conn.set_trace_callback(print)
-        cur = conn.cursor().execute(query, args)
-        if commit:
-            conn.commit()
-        return cur.fetchone() if one else cur.fetchall()
+	import sqlite3
 
+	DB_FILENAME = 'webgoat.db'
 
-def create_app():
+	def query_db(query, args=(), one=False, commit=False):
+	    with sqlite3.connect(DB_FILENAME) as conn:
+	        cur = conn.cursor().execute(query, args)
+	        if commit:
+	            conn.commit()
+	        return cur.fetchone() if one else cur.fetchall()
+
     app = Flask(__name__)
     app.secret_key = "aeZ1iwoh2ree2mo0Eereireong4baitixaixu5Ee"
 
@@ -49,3 +50,4 @@ def create_app():
         app.register_blueprint(ui.bp)
         app.register_blueprint(users.bp)
         return app
+
